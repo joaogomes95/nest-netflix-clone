7416e201253651f4363dd4aebb4569626748b15a
João Vitor Gomes <joaovitorgomes95@gmail.com>
2021-10-28 19:28:51 -0300

    first commit

---
+++ server/prisma/migrations/20211028011230_create_user/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "USer" (
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "displayName" TEXT NOT NULL,
+
+    CONSTRAINT "USer_pkey" PRIMARY KEY ("username")
+);
